a66f0cd06ec74fb92d40ed4d07bb29d2d7da39b4
Yaser Basravi <basraviyaserm@gmail.com>
2025-07-31 12:03:45 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
+
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('✅ Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+
+})
+.catch((err) => {
+  console.error('❌ MongoDB connection error:', err);
+});
